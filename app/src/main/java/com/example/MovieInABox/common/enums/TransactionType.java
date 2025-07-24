f0889a2bbaa597bba534a70bab585f6c8a99d652
Trịnh Ngọc Quang <trinhngocquang2004@gmail.com>
2025-07-24 17:06:01 +0700

    Add Model

--- app/src/main/java/com/example/MovieInABox/common/enums/TransactionType.java
+++
@@ -1,9 +0,0 @@
-package com.example.MovieInABox.common.enums;
-
-/**
- * Kiểu giao dịch: nạp (Credit) hoặc rút (Debit)
- */
-public enum TransactionType {
-    CREDIT, // nap tien
-    DEBIT   // rut tien
-}
