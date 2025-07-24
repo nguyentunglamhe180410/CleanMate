12ce1834fcb4d9ee43f630fbf19b91a2d04fa4cd
truong312 <lamnthe180410@fpt.edu.vn>
2025-07-24 17:38:49 +0700

    add theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.cleanmate"
+        applicationId = "com.example.MovieInABox"
         minSdk = 31
         targetSdk = 35
         versionCode = 1
